3f981192b2dd680e73ea814cc38a8822d707daf5
Pavel Timofeev <pavel.timofeev@quest.com>
2020-04-02 19:19:36 +0300

    save...

--- frontend/src/app/components/input-box/input-box.component.ts
+++ frontend/src/app/components/input-box/input-box.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-input-box',
   templateUrl: './input-box.component.html',
   styleUrls: ['./input-box.component.css']
 })
-export class InputBoxComponent implements OnInit {
+export class InputBoxComponent {
+
+  @Input()
+  label:string;
+
+  @Input()
+  placeholder:string;
+
+  @Input()
+  value:any;
 
   constructor() { }
-
-  ngOnInit() {
-  }
 
 }
--- frontend/src/app/view/configuration/configuration.component.ts
+++ frontend/src/app/view/configuration/configuration.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ConfigurationComponent implements OnInit {
 
+  url:string = "http://localhost";
+
   constructor() { }
 
   ngOnInit() {

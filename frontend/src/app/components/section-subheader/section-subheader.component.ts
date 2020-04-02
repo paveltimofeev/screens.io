fe33451504916b87c1dd8bd2c764d913c6117efe
Pavel Timofeev <pavel.timofeev@quest.com>
2020-04-02 18:41:46 +0300

    section subheader

--- frontend/src/app/components/section-subheader/section-subheader.component.ts
+++ frontend/src/app/components/section-subheader/section-subheader.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-section-subheader',
   templateUrl: './section-subheader.component.html',
   styleUrls: ['./section-subheader.component.css']
 })
-export class SectionSubheaderComponent implements OnInit {
+export class SectionSubheaderComponent {
+
+  @Input()
+  text:string;
 
   constructor() { }
-
-  ngOnInit() {
-  }
 
 }

c71fd7a44cddded0fc2e2eecd29813bbd5c84005
Pavel Timofeev <pavel.timofeev@quest.com>
2020-04-09 23:26:20 +0300

    Retry all requests exept 401/403/404

--- frontend/src/app/services/data-access.service.ts
+++ frontend/src/app/services/data-access.service.ts
@@ -1,24 +1,90 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
-import { map, take } from 'rxjs/operators';
+import { Observable, throwError, timer } from 'rxjs';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { catchError, finalize, mergeMap, retryWhen, take } from 'rxjs/operators';
+
+
+interface RetryOpts {
+  maxRetryAttempts?: number,
+  scalingDuration?: number,
+  excludedStatusCodes?: number[]
+}
+
+// https://www.learnrxjs.io/learn-rxjs/operators/error_handling/retrywhen
+export const genericRetryStrategy = ({ maxRetryAttempts = 3, scalingDuration = 1000, excludedStatusCodes = []}: RetryOpts = {}) => (attempts: Observable<any>) => {
+  return attempts.pipe(
+
+    mergeMap((error, i) => {
+
+      const retryAttempt = i + 1;
+      // if maximum number of retries have been met
+      // or response is a status code we don't wish to retry, throw error
+      if (
+        retryAttempt > maxRetryAttempts ||
+        excludedStatusCodes.find(e => e === error.status)
+      ) {
+        return throwError(error);
+      }
+
+      let delay = (retryAttempt + Math.random()) * scalingDuration;
+      console.log(`Attempt ${retryAttempt}: retrying in ${delay}ms`);
+      // retry after 1s, 2s, etc...
+      return timer(delay);
+    }),
+    finalize(() => {
+      //console.log('We are done!')
+    })
+  );
+};
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataAccessService {
 
+  retryOpts = {
+    maxRetryAttempts: 20,
+    scalingDuration: 2000,
+    excludedStatusCodes: [401, 403, 404]
+  };
+
   constructor(private http: HttpClient) { }
 
+  /// https://angular.io/guide/http#error-handling
+  handleError (error: HttpErrorResponse) {
+
+    if (error.error instanceof ErrorEvent) {
+      console.log('client side or network error')
+    }
+    else {
+      console.log(`Response error: StatusCode ${error.status}, ${error.message} `)
+    }
+
+    return throwError('Network error');
+  }
+
   get (url:string): Observable<any> {
-    return this.http.get(url, {withCredentials: true}).pipe(take(1));
+    return this.http.get(url, {withCredentials: true}).pipe(
+      take(1),
+      retryWhen(genericRetryStrategy(this.retryOpts)),
+      catchError(this.handleError)
+    );
   }
 
   post (url:string, body:any = {}): Observable<any> {
-    return this.http.post(url, body, {withCredentials: true}).pipe(take(1));
+    return this.http.post(url, body, {withCredentials: true}).pipe(
+      take(1),
+      retryWhen(genericRetryStrategy(this.retryOpts)),
+      catchError(this.handleError)
+    );
   }
 
   put (url:string, body:any = {}): Observable<any> {
-    return this.http.put(url, body, {withCredentials: true}).pipe(take(1));
+    return this.http.put(url, body, {withCredentials: true}).pipe(
+      take(1),
+      retryWhen(genericRetryStrategy(this.retryOpts)),
+      catchError(this.handleError)
+    );
   }
 }

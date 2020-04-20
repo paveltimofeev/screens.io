98f47b3a3a737dd222123ab82206d15cebe35229
Pavel Timofeev <pavel.timofeev@quest.com>
2020-04-21 02:08:59 +0300

    Refactoring

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -4,6 +4,16 @@ const VRT = require('../app_logic/vrt');
 
 const vrt = new VRT('test-tenant', 'test-user');
 
+const tryWrapper = async (req, res, action) => {
+
+    try {
+        await action()
+    }
+    catch (error) {
+        console.error('[API] Error', error)
+        res.status(500).send( { error: error.uiError || 'Error occurs' } )
+    }
+}
 
 router.post('/test/run', async function(req, res) {
 
@@ -11,21 +21,18 @@ router.post('/test/run', async function(req, res) {
         ...req.body,  // TODO: sanitize body
     };
 
-    try {
+    tryWrapper(req, res, async () => {
+
         const data = await VRT.create(req.context).run(opts)
         res.status(200).send( { data } )
-    }
-    catch (error) {
-        console.error('[API] Error', error)
-        res.status(500).send( { error: error.uiError || 'Error occurs' } )
-    }
+    })
 });
 
 router.post('/test/approvecase', async function(req, res) {
 
     const pair = req.body; // TODO: sanitize body
 
-    try {
+    tryWrapper(req, res, async () => {
 
         const result = await vrt.approveCase({
             reference: pair.reference,
@@ -43,13 +50,7 @@ router.post('/test/approvecase', async function(req, res) {
             res.status(500)
               .send( {message:'Cannot approve'} );
         }
-    }
-    catch (error) {
-        console.error('[VRT] Error approvecase', error)
-        res.status(500)
-          .send( {message:'Cannot approve. Error occurs'} )
-    }
-
+    })
 });
 
 
@@ -58,218 +59,161 @@ router.get('/test/report/:runId', async function(req, res) {
 
     const runId = req.params.runId; // TODO: sanitize
 
-    try {
+    tryWrapper(req, res, async () => {
+
         const report = await VRT.create(req.context).getReportByRunId(runId);
         res.status(200).send( {report} )
-    }
-    catch (error) {
-        console.error('[API] ' + req.path, error)
-        res.status(200).send( {error: 'Cannot get report'} );
-    }
+    })
 });
 
 router.get('/test/history', async function(req, res) {
 
-    try {
+    tryWrapper(req, res, async () => {
+
         const jobs = await VRT.create(req.context).getHistoryRecords()
         res.status(200).send( {jobs} )
-    }
-    catch (error) {
-        res.status(500).send( {error} )
-    }
+    })
 });
 
 // Delete history record
 router.delete('/test/history/:id', async function(req, res) {
 
-    try {
+    tryWrapper(req, res, async () => {
+
         const data = await VRT.create(req.context).deleteHistoryRecord(req.params.id)
         res.status(200).send( {data} )
-    }
-    catch (error) {
-        console.error('[API] Error: ' + req.path, error)
-        res.status(500).send( {error: 'Error occurs at ' + req.path} )
-    }
+    })
 });
 
 
 // Get scenario by Id
 router.get('/test/scenario/:id', async function(req, res) {
 
-    try {
+    tryWrapper(req, res, async () => {
 
-        const data = await VRT.create(req.context).getScenarioById(req.params.id)
-        res.status(200).send( {data} )
-    }
-    catch (error) {
-        console.error('[API] Error: ' + req.path, error)
-        res.status(500).send( {error: 'Error occurs at ' + req.path} )
-    }
+        const data = await VRT.create( req.context ).getScenarioById( req.params.id )
+        res.status( 200 ).send( { data } )
+    })
 });
 
 // Get scenarios
 router.get('/test/scenarios', async function(req, res) {
 
-    try {
-        const data = await VRT.create(req.context).getScenarios()
-        res.status(200).send( {data} )
-    }
-    catch (error) {
-        console.error('[API] Error: ' + req.path, error)
-        res.status(500).send( {error: 'Error occurs at ' + req.path} )
-    }
+    tryWrapper(req, res, async () => {
+
+        const data = await VRT.create( req.context ).getScenarios()
+        res.status( 200 ).send( { data } )
+    })
 });
 
 // Clone scenario
 router.post('/test/scenario/:id/clone', async function(req, res) {
 
-    try {
+    tryWrapper(req, res, async () => {
 
         const data = await VRT.create(req.context).cloneScenario(req.params.id, req.body)
         res.status(200).send( {data} )
-    }
-    catch (error) {
-        console.error('[API] Error: ' + req.path, error)
-        res.status(500).send( {error: 'Error occurs at ' + req.path} )
-    }
+    })
 });
 
 // Add scenario to favorite
 router.post('/test/scenario/:id/favorite', async function(req, res) {
 
-    try {
+    tryWrapper(req, res, async () => {
 
         const data = await VRT.create(req.context).addScenarioToFavorites(req.params.id)
         res.status(200).send( {data} )
-    }
-    catch (error) {
-        console.error('[API] Error: ' + req.path, error)
-        res.status(500).send( {error: 'Error occurs at ' + req.path} )
-    }
+    })
 });
 
 // Delete scenario from favorites
 router.delete('/test/scenario/:id/favorite', async function(req, res) {
 
-    try {
+    tryWrapper(req, res, async () => {
 
         const data = await VRT.create(req.context).removeScenarioFromFavorites(req.params.id)
         res.status(200).send( {data} )
-    }
-    catch (error) {
-        console.error('[API] Error: ' + req.path, error)
-        res.status(500).send( {error: 'Error occurs at ' + req.path} )
-    }
+    })
 });
 
 // Create scenario
 router.post('/test/scenario', async function(req, res) {
 
-    try {
+    tryWrapper(req, res, async () => {
 
         const data = await VRT.create(req.context).createScenario(req.body)
         res.status(200).send( {data} )
-    }
-    catch (error) {
-        console.error('[API] Error: ' + req.path, error)
-        res.status(500).send( {error: 'Error occurs at ' + req.path} )
-    }
+    })
 });
 
 // Update scenario
 router.put('/test/scenario/:id', async function(req, res) {
 
-    try {
+    tryWrapper(req, res, async () => {
 
         const data = await VRT.create(req.context).updateScenario(req.params.id, req.body)
         res.status(200).send( {data} )
-    }
-    catch (error) {
-        console.error('[API] Error: ' + req.path, error)
-        res.status(500).send( {error: 'Error occurs at ' + req.path} )
-    }
+    })
 });
 
 // Delete scenario
 router.delete('/test/scenario/:id', async function(req, res) {
 
-    try {
+    tryWrapper(req, res, async () => {
+
         const data = await VRT.create(req.context).deleteScenario(req.params.id)
         res.status(200).send( {data} )
-    }
-    catch (error) {
-        console.error('[API] Error: ' + req.path, error)
-        res.status(500).send( {error: 'Error occurs at ' + req.path} )
-    }
+    })
 });
 
 // Get viewport by Id
 router.get('/test/viewport/:id', async function(req, res) {
 
-    try {
+    tryWrapper(req, res, async () => {
 
         const data = await VRT.create(req.context).getViewportById(req.params.id)
         res.status(200).send( {data} )
-    }
-    catch (error) {
-        console.error('[API] Error: ' + req.path, error)
-        res.status(500).send( {error: 'Error occurs at ' + req.path} )
-    }
+    })
 });
 
 // Get viewport
 router.get('/test/viewports', async function(req, res) {
 
-    try {
+    tryWrapper(req, res, async () => {
 
         const data = await VRT.create(req.context).getViewports()
         res.status(200).send( {data} )
-    }
-    catch (error) {
-        console.error('[API] Error: ' + req.path, error)
-        res.status(500).send( {error: 'Error occurs at ' + req.path} )
-    }
+    })
 });
 
 // Create viewport
 router.post('/test/viewport', async function(req, res) {
 
-    try {
+    tryWrapper(req, res, async () => {
 
         const data = await VRT.create(req.context).createViewport(req.body)
         res.status(200).send( {data} )
-    }
-    catch (error) {
-        console.error('[API] Error: ' + req.path, error)
-        res.status(500).send( {error: 'Error occurs at ' + req.path} )
-    }
+    })
 });
 
 // Update viewport
 router.put('/test/viewport/:id', async function(req, res) {
 
-    try {
+    tryWrapper(req, res, async () => {
 
         const data = await VRT.create(req.context).updateViewport(req.params.id, req.body)
         res.status(200).send( {data} )
-    }
-    catch (error) {
-        console.error('[API] Error: ' + req.path, error)
-        res.status(500).send( {error: 'Error occurs at ' + req.path} )
-    }
+    })
 });
 
 // Delete viewport
 router.delete('/test/viewport/:id', async function(req, res) {
 
-    try {
+    tryWrapper(req, res, async () => {
+
         const data = await VRT.create(req.context).deleteViewport(req.params.id)
         res.status(200).send( {data} )
-    }
-    catch (error) {
-        console.error('[API] Error: ' + req.path, error)
-        res.status(500).send( {error: 'Error occurs at ' + req.path} )
-    }
+    })
 });
 
 

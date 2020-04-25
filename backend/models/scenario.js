a266a9cee9f4a21b93b774012bcc073ce80f7d57
Pavel Timofeev <pavel.timofeev@quest.com>
2020-04-26 02:23:53 +0300

    Fix selectors support

--- backend/models/scenario.js
+++ backend/models/scenario.js
@@ -5,7 +5,7 @@ const scenarioSchema = new Schema({
     /// BackstopJS fields
     label: String,
     url: String,
-    misMatchThreshold: Number, /// Decimal
+    misMatchThreshold: Number, /// Decimal 0-100. Test sensitivity, tolerance to difference
     readySelector: String,
     delay: Number,
     readyEvent: String,
@@ -15,6 +15,10 @@ const scenarioSchema = new Schema({
     scrollToSelector: String,
     hoverSelectors: Array,        /// String array
     postInteractionWait: Number,
+
+    selectors: Array,
+    selectorExpansion: Boolean, /// Default false, unused when selectors is empty
+    expect: Number, /// Default 0, unused when selectorExpansion=false
 
     meta_isFavorite: Boolean
 });

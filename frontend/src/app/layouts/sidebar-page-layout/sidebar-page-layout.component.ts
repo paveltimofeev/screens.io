a2971ca76c1045afff8fe9e75fc7f682bd77ae36
Pavel Timofeev <pavel.timofeev@quest.com>
2020-05-06 22:44:05 +0300

    refactoring - Frontend: create basic routes and responsive/mobile-compatible layouts wireframes (by Figma design)

--- frontend/src/app/layouts/sidebar-page-layout/sidebar-page-layout.component.ts
+++ frontend/src/app/layouts/sidebar-page-layout/sidebar-page-layout.component.ts
@@ -1,15 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-sidebar-page-layout',
   templateUrl: './sidebar-page-layout.component.html',
   styleUrls: ['./sidebar-page-layout.component.css']
 })
-export class SidebarPageLayoutComponent implements OnInit {
+export class SidebarPageLayoutComponent {
 
   constructor() { }
-
-  ngOnInit() {
-  }
-
 }

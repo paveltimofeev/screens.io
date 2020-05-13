6df33650d92e63663a3f1f9add59f98fb999cfca
Pavel Timofeev <pavel.timofeev@quest.com>
2020-05-14 02:25:59 +0300

    UI Kit: refactoring

--- frontend/src/app/ui-kit/data-search-button/data-search-button.component.ts
+++ frontend/src/app/ui-kit/data-search-button/data-search-button.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Input, Output, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-data-search-button',
@@ -7,8 +7,10 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 export class DataSearchButtonComponent  {
 
   value:string = '';
-
   editMode:boolean = false;
+
+  @ViewChild('el')
+  el:ElementRef;
 
   @Input()
   label:string;
@@ -24,6 +26,11 @@ export class DataSearchButtonComponent  {
 
     this.value = val.trim();
     this.changed.emit(this.value);
+  }
+
+  editModeHandler () {
+    this.editMode = true;
+    setTimeout(() => { this.el.nativeElement.focus() }, 100)
   }
 
   closeClickHandler () {

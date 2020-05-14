138a2049dba6f69a822fb662d8b146257ecf4b3b
Pavel Timofeev <pavel.timofeev@quest.com>
2020-05-15 01:17:33 +0300

    UI Kit: bind scenarios

--- frontend/src/app/view/scenarios/scenarios.component.ts
+++ frontend/src/app/view/scenarios/scenarios.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { select, Store } from '@ngrx/store';
+import { ActivatedRoute } from '@angular/router';
+import { NavigationService } from '../../services/navigation.service';
+import { selectLoading, selectScenarios, selectViewports } from '../configuration/store/configuration.selectors';
+import { refresh } from '../configuration/store/configuration.actions';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-scenarios',
@@ -7,13 +13,75 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ScenariosComponent implements OnInit {
 
-  constructor() { }
+  url:string = "http://localhost";
 
-  ngOnInit() {
+  isLoading$;
+  viewports$;
+  scenarios$;
+
+  constructor(private store: Store,
+              private route: ActivatedRoute,
+              private navigation: NavigationService
+  ) {}
+
+  ngOnInit () {
+
+    this.isLoading$ = this.store.pipe(select(selectLoading));
+    this.viewports$ = this.store.pipe(select(selectViewports)).pipe(
+      map( x => { return x.map( s => s.label) })
+    );
+    this.scenarios$ = this.store.pipe(select(selectScenarios));
+
+    this.refresh()
   }
 
-  testHandler(e:any, label:string='testHandler') {
-    console.log(label, e);
+  addScenarioHandler () {
+    // this.modals.addScenarioModal.open();
+    console.log('addScenarioHandler')
+  }
+  runFilteredScenariosHandler () {
+    console.log('runFilteredScenariosHandler')
+  }
+  removeFilteredScenariosHandler () {
+    console.log('removeFilteredScenariosHandler')
   }
 
+  refresh () {
+    this.store.dispatch(refresh({payload: {id: null}}));
+  }
+  search ($event) {
+    // this.modals.addScenarioModal.open();
+    console.log('search', $event)
+  }
+  showDifference ($event) {
+    // this.modals.addScenarioModal.open();
+    console.log('showDifference', $event)
+  }
+
+  selectScenarioHandler ($event) {
+    this.navigation.openScenario($event);
+  }
+  runScenarioHandler ($event) {
+    console.log('runScenarioHandler', $event)
+  }
+  deleteScenarioHandler ($event) {
+    console.log('deleteScenarioHandler', $event)
+  }
+  favoriteScenarioHandler ($event) {
+    console.log('favoriteScenarioHandler', $event)
+  }
+
+  changeStatusFilter_Passed ($event) {
+    console.log('changeStatusFilter_Passed', $event)
+  }
+  changeStatusFilter_Failed ($event) {
+    console.log('changeStatusFilter_Failed', $event)
+  }
+  changeViewportsFilter ($event) {
+    console.log('changeViewportsFilter', $event)
+  }
+
+  showMobileFilters () {
+    console.log('showMobileFilters')
+  }
 }

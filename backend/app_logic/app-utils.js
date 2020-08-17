073d867f71f0d11c149b54b51d397bee21aa7d0b
Pavel Timofeev <pavel.timofeev@quest.com>
2020-08-18 01:04:58 +0300

    refactoring

--- backend/app_logic/app-utils.js
+++ backend/app_logic/app-utils.js
@@ -1,18 +1,19 @@
-const path = require('path')
-const config = require('./configuration')
+const path = require('path');
+const config = require('./configuration');
 
-function uuidv4() {
+
+const uuidv4 = () => {
   return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
     var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
     return v.toString(16);
   });
-}
+};
 
-function uniqueOnly (value, index, self) {
+const uniqueOnly = (value, index, self) => {
   return self.findIndex(x => {
     return x.label === value.label && x.status === value.status
   }) === index;
-}
+};
 
 const validateScenario = (data) => {
 
@@ -33,7 +34,7 @@ const validateScenario = (data) => {
   allowStringArraysOnly(data, 'clickSelectors');
   allowStringArraysOnly(data, 'hoverSelectors');
   allowStringArraysOnly(data, 'selectors');
-}
+};
 
 const safeParse = (strData, defValue) => {
 
@@ -46,12 +47,13 @@ const safeParse = (strData, defValue) => {
   }
 };
 
-function skipPassedIfHasFailed (value, index, self) {
+const skipPassedIfHasFailed = (value, index, self) => {
   let found = self.findIndex(x => {
     return x.label === value.label && x.status === 'Failed'
-  })
+  });
   return  found  === index || found === -1;
-}
+};
+
 
 class FilePathsService {
 
